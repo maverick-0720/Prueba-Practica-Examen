54cb37baef8fd4e3c3710141ef719072f631679b
Roger Retana Mc Nally <rogerretanamc@gmail.com>
2020-11-15 20:42:39 -0600

    Prueba de examen

---
+++ src/main/java/com/cenfotec/crud/service/ArticleServiceImpl.java
@@ -0,0 +1,19 @@
+package com.cenfotec.crud.service;
+
+import com.cenfotec.crud.domain.Article;
+import com.cenfotec.crud.repo.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleServiceImpl implements ArticleService{
+
+	@Autowired
+	ArticleRepository articleRepo;
+	
+	
+	public void save(Article article) {
+		articleRepo.save(article);
+	}
+	
+}
